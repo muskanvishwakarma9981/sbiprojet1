5a6ffd308241a7ddc79e522ffdb20f63091cf7e8
muskanvishwakarma9981 <muskanvishwakarma9981@gmail.com>
2024-04-15 15:36:58 +0530

    first commit

---
+++ program.js
@@ -0,0 +1,31 @@
+//
+//let data = [{name: "pooja,city:"bpl},{nsme:"musskan", city: "sagar", }];
+
+//let data = [45,55,46,25,28]
+// for(let ind = 0; ind <data.length; ind++)
+// data[ind] = data[ind]+ 5;
+// console.log(data);
+
+// let newdata = data.map(function(curElement + 5,index){
+
+//     console.log(curELement + 5);
+// })
+
+
+// map(function(curElement){
+//     console.log(curELement + 5);
+// })
+// let users = [{name: "mus", city : "bpl"},{name: "sjjh", city: "gshh"}]
+// users.map((currEL, index)=>{
+//     console.log(currEl.name)
+// })
+//console.log("hhh")
+ 
+let data = [ 
+    { name : "muskan", city : "bhopal"},
+    { name : "suhani", city : "bhopal"}
+]
+
+data.map((currEl,index) => {
+    console.log(currEl.index)
+})
